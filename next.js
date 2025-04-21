12b6be493e59b97c872a9224d69e97f640c45e02
sayheysob <sunanadjiyulian@gmail.com>
2025-04-21 21:45:27 +0700

    Update next.js

--- next.js
+++ next.js
@@ -1,2 +1,8 @@
-npx create-next-app@latest pantry-report
-cd pantry-report
+/pages
+  /api
+    /submit.js       <-- Endpoint untuk terima data & kirim ke Discord
+  index.js           <-- Halaman form input
+/utils
+  webhook.js         <-- Fungsi kirim webhook ke Discord
+/data
+  stock.json         <-- (opsional) File JSON sebagai "stok sementara"
