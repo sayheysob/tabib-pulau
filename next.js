3e13c8e47376e100f3fd5d5308b89949709a21b2
sayheysob <sunanadjiyulian@gmail.com>
2025-05-27 23:18:39 +0900

    Update next.js

--- next.js
+++ next.js
@@ -1,8 +1,1 @@
-/pages
-  /api
-    /submit.js       <-- Endpoint untuk terima data & kirim ke Discord
-  index.js           <-- Halaman form input
-/utils
-  webhook.js         <-- Fungsi kirim webhook ke Discord
-/data
-  stock.json         <-- (opsional) File JSON sebagai "stok sementara"
+
